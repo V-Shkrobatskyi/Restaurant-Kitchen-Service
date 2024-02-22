8eef982b02d2288409763af6f0aca300725cf86b
Vitalii <vetali5700gmail.com>
2024-02-22 22:54:45 +0200

    add dish-type-list path to urls, add DishTypesListView to views, add dish_types_list.html to templates, add url into sidebar.html

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from kitchen.views import index
-
+from kitchen.views import index, DishTypesListView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypesListView.as_view(), name="dish-type-list"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from kitchen.models import DishType, Dish, Cook
 
@@ -17,3 +18,9 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_visits": num_visits + 1,
     }
     return render(request, "kitchen/index.html", context)
+
+
+class DishTypesListView(generic.ListView):
+    model = DishType
+    template_name = "kitchen/dish_types_list.html"
+    context_object_name = "dish_type_list"

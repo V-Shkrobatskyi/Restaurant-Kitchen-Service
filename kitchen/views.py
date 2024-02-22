1d8853ec57cfca24ff31ca3532dac76134f204c7
Vitalii <vetali5700gmail.com>
2024-02-22 21:52:16 +0200

    add kitchen and accounts path to urls.py in project, add urls.py to application and configure index.html page with sidebar, add index in views, add templates settings directory to the settings

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import DishType, Dish, Cook
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_visits": num_visits + 1,
+    }
+    return render(request, "kitchen/index.html", context)

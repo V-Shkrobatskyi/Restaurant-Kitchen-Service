6b822df9ce08c3aebb4148aa9b60e447dbc2aaed
Vitalii <vetali5700gmail.com>
2024-03-05 22:35:45 +0200

    add login and loguot
    add regisstration dir in templates with login and logged_out html files, add login_redirect_url to settings, add @login_required and LoginRequiredMixin in app views

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
@@ -5,6 +7,7 @@ from django.views import generic
 from kitchen.models import DishType, Dish, Cook
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_dishes = Dish.objects.count()
     num_cooks = Cook.objects.count()
@@ -20,27 +23,27 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "kitchen/index.html", context)
 
 
-class DishTypeListView(generic.ListView):
+class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
     template_name = "kitchen/dish_type_list.html"
     context_object_name = "dish_type_list"
     paginate_by = 5
 
 
-class CookListView(generic.ListView):
+class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
 
 
-class CookDetailView(generic.DetailView):
+class CookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Cook
 
 
-class DishListView(generic.ListView):
+class DishListView(LoginRequiredMixin, generic.ListView):
     model = Dish
     paginate_by = 5
     queryset = Dish.objects.select_related("dish_type")
 
 
-class DishDetailView(generic.DetailView):
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
     model = Dish

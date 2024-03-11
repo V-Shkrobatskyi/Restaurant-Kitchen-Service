1a02769fa563a278d5d2679abcdf3ded5319ce76
Vitalii <vetali5700gmail.com>
2024-03-11 22:02:01 +0200

    add cook search
    add CookSearchForm in forms.py, modify CookListView in views for search, add search.html in templates, add search include in cook_list.html

--- kitchen/views.py
+++ kitchen/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from kitchen.forms import CookCreationForm, CookExperienceUpdateForm, DishForm
+from kitchen.forms import CookCreationForm, CookExperienceUpdateForm, DishForm, CookSearchForm
 from kitchen.models import DishType, Dish, Cook
 
 
@@ -55,6 +55,23 @@ class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
 class CookListView(LoginRequiredMixin, generic.ListView):
     model = Cook
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all()
+        form = CookSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
 
 
 class CookDetailView(LoginRequiredMixin, generic.DetailView):

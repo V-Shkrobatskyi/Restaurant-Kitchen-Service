ce589c5cbeddb4cb25981af5488709470b062db2
Vitalii <vetali5700gmail.com>
2024-03-14 21:50:54 +0200

    add templates for home page
    add layouts dir in templates and new templates in includes, add settings for them, fix logout for post method

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "kitchen.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -119,6 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
